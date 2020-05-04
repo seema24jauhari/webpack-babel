71926c4aa8cb152d11d3c3b2c38511f456372522
seema <seema.burgeon@gmail.com>
2020-05-05 01:12:02 +0530

    setup webpack with html loader

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
-import '../css/bootstrap.min.css';
+import '../plugins/bootstrap/css/bootstrap.min.css';
 import '../css/style.css';
+import '../plugins/bootstrap/js/bootstrap.min.js';
 console.log("start count");
-for (var i = 1; i <= 10; i++) {
-	console.log(i);
-}
+$(document).ready(function(){
+	for (var i = 1; i <= 10; i++) {
+		console.log(i);
+	}
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,44 @@
 var path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 
+
+ 
 module.exports = {
 	entry: './js/app.js',
 	output: {
 		path: path.resolve(__dirname,'dist'),
 		filename: 'bundle.js',
-		publicPath: '/dist'
+		//publicPath: '/dist'
 	},
 	module:{
 		rules: [
 			{
 				test: /\.css$/i,
 				use: [MiniCssExtractPlugin.loader,'css-loader']
-			}
+			},
+			{
+		        test: /\.(ttf|eot|woff(2)|woff|woff2|svg)$/i,
+		        use: [
+		          {
+		            loader: 'file-loader',
+		          },
+		        ],
+		    },
+		    {
+		        test: /\.(png|jpg|pe?g|gif)$/,
+		        use: [
+		          {
+		            loader: 'file-loader',
+		            options: {
+		            	name: '[name].[ext]',
+		            	outputPath:'img/',
+		            	publicPath:'img/'
+		            }
+		          },
+		        ],
+		    },
 		]
 	},
 	plugins: [
@@ -23,5 +48,13 @@ module.exports = {
           filename: '[name].css',
           chunkFilename: '[id].css',
         }),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'index.html'
+        }),
+        new webpack.ProvidePlugin({
+		  $: 'jquery',
+		  jQuery: 'jquery'
+		})
      ]
 };
